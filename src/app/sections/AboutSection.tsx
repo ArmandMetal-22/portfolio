179161a2d3f3fac7a8acc2f4a16740cf3358c9e3
Armand Metelerkamp <armandmet22@gmail.com>
2025-07-03 21:40:45 +0200

    test commit

--- src/app/sections/AboutSection.tsx
+++ src/app/sections/AboutSection.tsx
@@ -2,6 +2,7 @@ export default function AboutSection() {
   return (
     <section id="about" className="scroll-mt-24 min-h-screen p-8 bg-gray-50">
       <h2 className="text-3xl font-semibold mb-4">About Me</h2>
+      <h2>test</h2>
       <p className="text-base leading-relaxed">
         I’m a passionate developer with 4+ years of experience working on real-world software in CRM, accounting, and automation.
         I love building clean, scalable systems that solve real problems.
